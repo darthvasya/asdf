bb01edacb16eb833b41051314f5aaca11c33ff92
vasyamakarchuk <vasyamakarchuk@outlook.com>
2017-11-12 22:46:17 +0300

    sdadsa

--- src/app/shared/configs/app.config.ts
+++ src/app/shared/configs/app.config.ts
@@ -3,7 +3,7 @@ import {OpaqueToken} from "@angular/core";
 export let APP_CONFIG = new OpaqueToken('app.config');
 
 export const AppConfig: any = {
-    apiUrl: "http://165.227.168.147",
-    apiEndpoint: "http://165.227.168.147/api",
-    apiEndpointVasyaTest: "http://localhost:52108/api"
+    apiUrl: "https://165.227.168.147",
+    apiEndpoint: "https://165.227.168.147/api",
+    apiEndpointVasyaTest: "https://localhost:52108/api"
 };
