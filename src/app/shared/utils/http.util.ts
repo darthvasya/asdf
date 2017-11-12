27d4794d3e4d2b1772d1cd79d114e535d58d6e04
vasyamakarchuk <vasyamakarchuk@outlook.com>
2017-11-12 22:12:42 +0300

    dafdsa

--- src/app/shared/utils/http.util.ts
+++ src/app/shared/utils/http.util.ts
@@ -3,13 +3,22 @@ import { Injectable } from "@angular/core";
 
 export class HttpUtil {
 
+    private static access_token = "";
+
+    constructor() {
+        const value = {
+            access_token: ""
+        };
+        localStorage.setItem('shop-app-user-data', JSON.stringify(value));
+    }
+
     static REQUEST_OPTIONS_WITH_CONTENT_TYPE_JSON = new RequestOptions({
         headers: new Headers({
             'Accept': 'application/json',
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '**',
             'Access-Control-Allow-Methods': '**',
-            'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('shop-app-user-data')).access_token ? null : ""
+            'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('shop-app-user-data'))['access_token']
         })
     });
 
@@ -18,7 +27,7 @@ export class HttpUtil {
             'Accept': 'application/json',
             'Access-Control-Allow-Origin': '**',
             'Access-Control-Allow-Methods': '**',
-            'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('shop-app-user-data')).access_token ? null : ""
+            'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('shop-app-user-data'))['access_token']
         })
     });
 }
