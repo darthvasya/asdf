67f7ff03cde3ef0d7eaba8b3b691aea8d18a5c53
vasyamakarchuk <vasyamakarchuk@outlook.com>
2017-11-12 21:11:11 +0300

    fix

--- src/app/shared/components/item/item-description/item-description.component.ts
+++ src/app/shared/components/item/item-description/item-description.component.ts
@@ -13,7 +13,7 @@ export class ItemDescriptionComponent implements OnInit {
 
     @Output() onDeleted = new EventEmitter<number>();
 
-    private API_ROUTE: string = `${this.config.apiUrl}`;
+    public API_ROUTE: string = `${this.config.apiUrl}`;
 
     constructor( @Inject(APP_CONFIG) private config: any) {
     }
