6116a685b9e4734ae7965cbc0ab78236a09baa2a
DimaRykunov <dima.rikunov@gmail.com>
2018-01-24 23:31:13 +0300

    issue8

---
+++ src/app/layout/instruction/instruction.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "app-instruction",
+    templateUrl: "./instruction.component.html",
+    styleUrls: ["./instruction.component.css"]
+})
+export class InstructionComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {
+
+    }
+}
