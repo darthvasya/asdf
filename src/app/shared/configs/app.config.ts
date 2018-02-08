9e709c4c422e32e03a8e607601cd277903c77f88
vasyamakarchuk <vasyamakarchuk@outlook.com>
2018-02-08 22:59:12 +0300

    s

--- src/app/shared/configs/app.config.ts
+++ src/app/shared/configs/app.config.ts
@@ -3,6 +3,6 @@ import {OpaqueToken} from "@angular/core";
 export let APP_CONFIG = new OpaqueToken('app.config');
 
 export const AppConfig: any = {
-    apiEndpoint: "https://prod.api.suvorov.co/api",
-    endpoint: "https://prod.api.suvorov.co"
+    apiEndpoint: "https://dev-api.suvorov.co/api",
+    endpoint: "https://dev-api.suvorov.co"
 };
